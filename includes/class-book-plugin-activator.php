a913f45545fd251e5c7fa8a61abb6d7b5842d72b
Tejas <tejas.patle@hbwsl.com>
2022-04-06 14:52:05 +0530

    final commit

---
+++ includes/class-book-plugin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://localhost/WordPress/
+ * @since      1.0.0
+ *
+ * @package    Book_Plugin
+ * @subpackage Book_Plugin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Book_Plugin
+ * @subpackage Book_Plugin/includes
+ * @author     Tejas Patle <tejaspatle77@gmail.com>
+ */
+class Book_Plugin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-book-plugin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://localhost/WordPress/
+ * @since      1.0.0
+ *
+ * @package    Book_Plugin
+ * @subpackage Book_Plugin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Book_Plugin
+ * @subpackage Book_Plugin/includes
+ * @author     Tejas Patle <tejaspatle77@gmail.com>
+ */
+class Book_Plugin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
